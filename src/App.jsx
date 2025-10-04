c661bdd8359d1c2b5c52ac18c392a68cc51205f2
mohy-walid <mohy.walid86@gmail.com>
2025-10-04 22:18:12 +0300

    Fix imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,24 @@
-import React from 'react'
-import { createBrowserRouter ,RouterProvider} from 'react-router-dom'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Home from './Home/Home';
 import About from './About/About';
-import Contact from './Contact/Contact';
-import Portfolio from '../src/Portfolio/Portfolio';
+import Contact from './contact/Contact';
+import Portfolio from './Portfolio/Portfolio';
 import Layout from './layout/Layout';
 
 export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Home /> },
+        { path: '/About', element: <About /> },
+        { path: '/Contact', element: <Contact /> },
+        { path: '/Portfolio', element: <Portfolio /> },
+      ],
+    },
+  ]);
 
-  const router=createBrowserRouter ([
-{
-  path:'/',
-  element:<Layout/>,
-  children:[ 
-    {path:'/',element:<Home/>},
-    {path:'/About',element:<About/>},
-    { path:'/Contact',element:<Contact/>},
-    {path:'/Portfolio', element:<Portfolio/>},
-  ]
-}
-
-
-  ]) 
-  return (
-    <>
- 
-      <RouterProvider router={router}/>
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
